1177a5c2cccb5a42f16507c3a9ecf789ffb9fa91
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-05-03 12:19:23 +1000

    Начало написания логики бота

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import telebot
 import engine.kernel as Core
 
 kernel = Core.Kernel()
+
+print(f"Token: \"{kernel.getToken()}\"")
+
+bot = telebot.TeleBot(kernel.getToken())
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, "Приветствую!")
+
+@bot.message_handler(content_types='text')
+def answer(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
