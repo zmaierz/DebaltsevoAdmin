572e1821b6e6aa3c335a282911eff5c3014382ef
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-05-03 11:27:41 +1000

    Написание логики получения конфигов

--- engine/kernel.py
+++ engine/kernel.py
@@ -3,7 +3,30 @@ import configparser
 class Kernel:
     def __init__(self):
         self.kernelConfig = configparser.ConfigParser()
+        self.DBConfig = configparser.ConfigParser()
         self.kernelConfig.read('engine/config/kernel.conf')
+        self.DBConfig.read('engine/config/db.conf')
         self.botToken = self.kernelConfig.get("BOT", "token")
+        self.webDBConfig = {
+            "username" : self.DBConfig.get("WEB", "username"),
+            "password" : self.DBConfig.get("WEB", "password"),
+            "hostname" : self.DBConfig.get("WEB", "hostname"),
+            "database" : self.DBConfig.get("WEB", "database"),
+            "port" : self.DBConfig.get("WEB", "port"),
+        }
+        if (self.DBConfig.get("BOT", "as-web") == "true"):
+            self.botDBConfig = self.webDBConfig
+        else:
+            self.botDBConfig = {
+                "username" : self.DBConfig.get("BOT", "username"),
+                "password" : self.DBConfig.get("BOT", "password"),
+                "hostname" : self.DBConfig.get("BOT", "hostname"),
+                "database" : self.DBConfig.get("BOT", "database"),
+                "port" : self.DBConfig.get("BOT", "port"),
+            }
     def getToken(self):
         return self.botToken
+    def getWebDBConfig(self):
+        return self.webDBConfig
+    def getBotDBConfig(self):
+        return self.botDBConfig
