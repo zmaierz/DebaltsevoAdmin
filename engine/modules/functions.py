c3a09702f07a0345e461fc9f8d790777de8b5d26
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-05-03 15:42:12 +0000

    Реализация метода удаления файлов

--- engine/modules/functions.py
+++ engine/modules/functions.py
@@ -1,5 +1,10 @@
+import os
 from transliterate import translit, get_available_language_codes
 
 def translitText(text):
     out = translit(text, 'ru', reversed=True)
     return out
+
+def deleteFile(path):
+    if (os.path.exists(path)):
+        os.remove(path)
