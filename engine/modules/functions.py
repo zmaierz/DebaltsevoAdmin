b4620185809bc666bc4734cef6b9f8a3844d08eb
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-05-03 17:18:56 +0000

    Реализация метода удаления содержимого из папки

--- engine/modules/functions.py
+++ engine/modules/functions.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import platform
 from transliterate import translit, get_available_language_codes
 
 def translitText(text):
@@ -19,3 +21,12 @@ def writeFileContent(path, data):
     file = open(path, "w")
     file.write(data)
     file.close()
+
+def deleteDirectoryContent(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            pass
