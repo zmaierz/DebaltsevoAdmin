93137f092123056bcb54edbec54e2b4c74bcbd50
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-05-03 12:38:59 +1000

    Создание модуля с текстом сообщений

---
+++ engine/modules/messages_text.py
@@ -0,0 +1,8 @@
+def getMainMenuButtons():
+    return {
+        "createNewPage" : "Создание страницы",
+        "Categories" : "Категории",
+        "News" : "Новости",
+        "Settings" : "Настройки",
+        "Logs" : "Логи"
+    }
