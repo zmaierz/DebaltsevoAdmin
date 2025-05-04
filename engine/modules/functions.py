a16a6f5becde263c9897e21797f1b690c96f9790
Nikita Semenov <nikitalsemenov00@gmail.com>
2025-05-04 08:12:30 +0000

    Реализация функции получения ID со смещением

--- engine/modules/functions.py
+++ engine/modules/functions.py
@@ -1,11 +1,23 @@
 import os
-import shutil
-import platform
 from transliterate import translit, get_available_language_codes
 
 def translitText(text):
     out = translit(text, 'ru', reversed=True)
     return out
+
+def getIDWithOffset(call, startPlace):
+    idOffset = 0
+    for i in range(startPlace, len(call)):
+        if (call[i] == "-"):
+            break
+        idOffset += 1
+    id = ""
+    if (idOffset != 0):
+        for i in range(0, idOffset):
+            id += call[startPlace + i]
+    else:
+        id = call[startPlace]
+    return id, idOffset
 
 def deleteFile(path):
     if (os.path.exists(path)):
